1fae7eaf997a1fabe71eddfe2c244ddf41e600b6
Yogita Bhargava <79744488+yogitabhargava02@users.noreply.github.com>
2023-11-14 23:06:18 +0530

    latest

--- server/index.js
+++ server/index.js
@@ -13,3 +13,37 @@ app.use('/api/customer', customerRoutes);
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
+
+
+// const express = require('express');
+// const NodeGeocoder = require('node-geocoder');
+
+// const app = express();
+// const PORT = 3000;
+// const geocoder = NodeGeocoder({
+//   provider: 'openstreetmap',
+// });
+
+// app.get('/geocode', (req, res) => {
+//   const address = 'Maruti Nagar, Bareli, Madhya Pradesh, India'; 
+//   geocoder.geocode(address)
+//     .then((data) => {
+//       if (!data.length) {
+//         return res.status(500).json({ error: 'Geocoding failed' });
+//       }
+
+//       const location = {
+//         latitude: data[0].latitude,
+//         longitude: data[0].longitude,
+//       };
+//       res.json(location);
+//     })
+//     .catch((err) => {
+//       console.error('Geocoding failed:', err);
+//       res.status(500).json({ error: 'Geocoding failed' });
+//     });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
--- server/routes/customerRoute.js
+++ server/routes/customerRoute.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const {customerRegister}=require('../controllers/auth/Customer/customerRegister');
 const {customerLogin}=require('../controllers/auth/Customer/customerLogin');
-
+const {customerProfile}=require('../controllers/auth/Customer/customerProfile')
 
 
 
 router.post('/cregister', customerRegister);
 router.post('/clogin', customerLogin);
+// router.post('/cprofile', customerProfile)
 module.exports = router;
