2f67ecb8b5936e45c9d68399505b380119b59d5a
Yogita Bhargava <79744488+yogitabhargava02@users.noreply.github.com>
2023-11-19 12:20:36 +0530

    reset

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ require("./config/database");
 app.use(express.json());
 app.use('/api/milkman', milkmanRoutes);
 app.use('/api/customer', customerRoutes);
+app.get('/reset-password', (req, res) => {
+    // Your logic for handling the reset password page
+    res.send('Reset Password Page'); // This can be your HTML page or template
+  });
+  
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
--- server/routes/customerRoute.js
+++ server/routes/customerRoute.js
@@ -4,7 +4,14 @@ const router = express.Router();
 const { customerRegister } = require('../controllers/auth/Customer/customerRegister');
 const { customerLogin } = require('../controllers/auth/Customer/customerLogin');
 const milkmanListController = require('../controllers/auth/Customer/milkmanListController'); 
+const {initiateForgotPassword} = require('../controllers/auth/Customer/customerForgot');
+const customerResetPasswordController = require('../controllers/auth/Customer/customerReset');
 
+// Define the route to reset the password for customers
+router.post('/reset-password', customerResetPasswordController.resetPassword);
+
+// Define the route to initiate the forgot password process for customers
+router.post('/forgot-password', initiateForgotPassword);
 router.post('/cregister', customerRegister);
 router.post('/clogin', customerLogin);
 router.get('/milkman-list', milkmanListController.getAllMilkmanProfiles); 
