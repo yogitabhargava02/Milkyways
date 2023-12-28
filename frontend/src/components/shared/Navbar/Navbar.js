87104c7121d715168f43efaea6b42deeced84eec
Anushka Agrawal <agrawalanushka512@gmail.com>
2023-12-29 03:12:44 +0530

    fixed

--- frontend/src/components/shared/Navbar/Navbar.js
+++ frontend/src/components/shared/Navbar/Navbar.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Login from '../../../assets/logo.png';
 
 const NavBar = () => {
   return (
-    <nav className="bg-white-800 p-4">
+    <nav className="bg-opacity-90 backdrop-blur-md p-3">
       <div className="container mx-auto flex items-center justify-between">
-       
+
         <div className="text-white font-bold text-xl">
-          <span className="text-yellow-500">Milk</span> Delivery
+          <img src={Login} alt="" style={{ width: "100px" }} />
+          {/* <span className="text-yellow-500">Dairy</span> To Door */}
         </div>
 
         <div className="space-x-4">
-          <Link to="/clogin" className="text-pink bg-green hover:text-yellow-500 focus:outline-none">
+          <Link to="/clogin" className="text-white hover:text-black-500 focus:outline-none">
             Login
           </Link>
 
-          <button className="bg-yellow-500 text-gray-800 px-4 py-2 rounded-md hover:bg-yellow-600 focus:outline-none">
-          <Link to="/mlogin" className="text-white hover:text-yellow-500 focus:outline-none">
-          Are you a milkman?
-          </Link>
+          <button className="bg-white-500 text-black-800 px-6 py-2 rounded-md hover:bg-blue-600 focus:outline-none">
+            <Link to="/mlogin" className="text-white hover:text-white-500 focus:outline-none">
+              Are you a milkman?
+            </Link>
           </button>
         </div>
       </div>
